983fb7d22eb12496c40bdd2372692c5a2f025634
lmx <limingxu1985@gmail.com>
2021-11-10 18:23:03 +0800

    add react-router-dom for routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React, { useState } from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import User from './User'
 
 function App() {
   const [userName, updateUserName] = useState('')
+  const [users, updateUsers] = useState('')
+  async function getUsers(userId) {
+    const res = await fetch(`/api/users/${userId}`).catch((err) =>
+      console.log(err)
+    )
+    console.log(res)
+    console.log(res.status)
+    if (res.status === 200) {
+      const data = await res.json().catch((err) => console.log(err))
+      console.log(data[0].id, data[0].email)
+      updateUsers(res)
+    }
+  }
   return (
     <div>
       <h1 data-testid="test-username-header">Vite-React Demo App</h1>
@@ -14,6 +29,26 @@ function App() {
         onChange={(e) => updateUserName(e.target.value)}
       />
       <div data-testid="test-username-display">user: {userName}</div>
+      <button onClick={() => getUsers(userName)}>button</button>
+      <div>
+        <h1>React Router</h1>
+        <BrowserRouter>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/user/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/user/register">Register</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<div>Hello</div>} />
+            <Route path="/user/*" element={<User />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   )
 }
---
+++ src/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+      <h3>Regiser</h3>
+    </div>
+  )
+}
+
+export default Register
