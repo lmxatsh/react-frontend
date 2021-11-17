8fca132ba1fb83e3a35e9cb4ee067be91bbfcce8
mingxu.li <limingxu1985@gmail.com>
2021-11-18 00:52:16 +0800

    modify .eslintrc.js and add lint in package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,17 +16,23 @@ module.exports = {
   },
   plugins: ['react', 'testing-library'],
   ignorePatterns: ['*.html'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    "testing-library/await-async-query": "error",
-    "testing-library/no-await-sync-query": "error",
-    "testing-library/no-debugging-utils": "warn",
-    "testing-library/no-dom-import": "off"
+    'testing-library/await-async-query': 'error',
+    'testing-library/no-await-sync-query': 'error',
+    'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'off',
   },
-  "overrides": [
+  overrides: [
     {
-      "files": ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-      "extends": ["plugin:testing-library/react"]
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
     },
+  ],
 }
